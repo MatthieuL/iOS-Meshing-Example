221bcb60018afc89ada1eda2cb086e836741937c
MatthieuL <lefebvre.mtth@gmail.com>
2018-06-28 21:10:16 +0200

    first update

--- iOSMeshing/ViewController.swift
+++ iOSMeshing/ViewController.swift
@@ -9,111 +9,42 @@
 import UIKit
 import MultipeerConnectivity
 
-class ViewController: UIViewController, MCSessionDelegate, MCBrowserViewControllerDelegate {
+enum DataType: UInt32 {
+    case string = 1
+}
 
-    private var peerID: MCPeerID!
-    private var mcSession: MCSession!
-    private var mcAdvertiserAssistant: MCAdvertiserAssistant!
+class ViewController: UIViewController, MultiPeerDelegate {
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        peerID = MCPeerID(displayName: UIDevice.current.name + " " + UIDevice.current.localizedModel)
-        mcSession = MCSession(peer                  : peerID,
-                              securityIdentity      : nil,
-                              encryptionPreference  : .required)
-        mcSession.delegate = self
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        MultiPeer.instance.initialize(serviceType: "demo-app")
+        MultiPeer.instance.autoConnect()
+        MultiPeer.instance.delegate = self
     }
     
-    // MARK : MCSession
-    
-    func session(_ session: MCSession,
-                 didReceive stream: InputStream,
-                 withName streamName: String,
-                 fromPeer peerID: MCPeerID)
-    {}
-    
-    func session(_ session: MCSession,
-                 didStartReceivingResourceWithName resourceName: String,
-                 fromPeer peerID: MCPeerID,
-                 with progress: Progress)
-    {}
-    
-    func session(_ session: MCSession,
-                 didFinishReceivingResourceWithName resourceName: String,
-                 fromPeer peerID: MCPeerID,
-                 at localURL: URL?,
-                 withError error: Error?)
-    {}
-    
-    func session(_ session: MCSession,
-                 didReceive data: Data,
-                 fromPeer peerID: MCPeerID)
-    {
-//        if let dictionary = Dictionary(data) {//UIImage(data: data) {
-//            DispatchQueue.main.async {
-//                // do something with the image
-//            }
-//        }
-        print(String(data:data, encoding: .utf8))
+    @IBAction func sendData() {
+        MultiPeer.instance.send(object: "Hello World!", type: DataType.string.rawValue)
     }
     
-    func session(_ session: MCSession,
-                 peer peerID: MCPeerID,
-                 didChange state: MCSessionState)
-    {
-        switch state {
-        case MCSessionState.connected:
-            print("Connected: \(peerID.displayName)")
+    func multiPeer(didReceiveData data: Data, ofType type: UInt32) {
+        switch type {
+        case DataType.string.rawValue:
+            let string = data.convert() as! String
+            // do something with the received string
+            break;
             
-        case MCSessionState.connecting:
-            print("Connecting: \(peerID.displayName)")
+        case DataType.image.rawValue:
+            let image = UIImage(data: data)
+            // do something with the received UIImage
+            break;
             
-        case MCSessionState.notConnected:
-            print("Not Connected: \(peerID.displayName)")
+        default:
+            break;
         }
     }
     
-    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
-        dismiss(animated: true)
-    }
-    
-    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
-        dismiss(animated: true)
-    }
-    
-    func startHosting(action: UIAlertAction!) {
-        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-kb", discoveryInfo: nil, session: mcSession)
-        mcAdvertiserAssistant.start()
-    }
-    
-    func joinSession(action: UIAlertAction!) {
-        let mcBrowser = MCBrowserViewController(serviceType: "hws-kb", session: mcSession)
-        mcBrowser.delegate = self
-        present(mcBrowser, animated: true)
-    }
-    
-    func sendImage(data: Data) {
-        if mcSession.connectedPeers.count > 0 {
-            do {
-                try mcSession.send(data,
-                                   toPeers: mcSession.connectedPeers, with: .reliable)
-            } catch let error as NSError {
-                let ac = UIAlertController(title            : "Send error",
-                                           message          : error.localizedDescription,
-                                           preferredStyle   : .alert)
-                ac.addAction(UIAlertAction(title: "OK",
-                                           style: .default))
-                present(ac, animated: true)
-            }
-        }
+    func multiPeer(connectedDevicesChanged devices: [String]) {
     }
 }
 
